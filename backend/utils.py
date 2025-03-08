edbbe5c5d36600979c49f083e03a1ec47e875261
tushar <tusharamalalia@gmail.com>
2025-03-08 19:52:53 +0530

    remove .txt from end of file name

--- backend/utils.py
+++ backend/utils.py
@@ -7,5 +7,7 @@ def extract_number(name):
     return int(match.group(1)) if match else float('inf')  # Assign large number if no match
 
 def get_stripped_name(name):
-    """Remove numeric prefix from a name"""
-    return re.sub(r"^\d+_", "", name)
+    """Remove numeric prefix from a name and .txt from the end"""
+    name =  re.sub(r"^\d+_", "", name)
+    name =  re.sub(r"\.txt$", "", name)
+    return name
